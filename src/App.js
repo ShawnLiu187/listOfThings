5d9e449285fee24c9bce080db528232ec5cbbbfc
ShawnLiu187 <31958595+ShawnLiu187@users.noreply.github.com>
2018-10-16 21:21:01 -0400

    Moved Lists to the root level

--- src/App.js
+++ src/App.js
@@ -3,11 +3,36 @@ import Header from './components/Header';
 import ListContainer from './components/ListContainer';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {players: [
+        {id: 1, name: "Guo Ailun", position: "Point Guard"},
+        {id: 2, name: "Yi Jianlian", position: "Power Foward"},
+        {id: 3, name: "Yao Ming", position: "Center"}
+    ]}
+}
+
+deletePlayer = (playerId) => {
+  let newPlayers = this.state.players.filter((item) => {
+      if(playerId != item.id){
+          return true;
+      }
+  });
+  this.setState({players: newPlayers});
+}
+
+addPlayer = (newPlayer) => {
+  let newPlayers = this.state.players;
+  newPlayers.push(newPlayer);
+  this.setState({players: newPlayers});
+}
+
+
   render() {
     return (
         <Fragment>
           <Header />
-          <ListContainer />
+          <ListContainer players={this.state.players} addPlayer={this.addPlayer} deletePlayer={this.deletePlayer}/>
         </Fragment>
     );
   }
